e76323c284367e05c152676b3b6ebe84e6b7e144
Farhad <farhad9801@gmail.com>
2019-02-14 22:24:19 +0400

    specify file name and path

--- lib/ortega/http.rb
+++ lib/ortega/http.rb
@@ -1,10 +1,19 @@
 require 'net/http'
+require 'active_support/core_ext/hash/indifferent_access'
 
 module Ortega
   module HTTP
     def download(url, options = {}, &block)
+      url = url.insert(0, 'http://') unless url.match(/http/) 
+      options = options.with_indifferent_access
       uri = URI(url)
-      File.new(uri.path, "wb") do |file|
+      
+      path = File.join(
+        "#{options[:path] ? File.expand_path(options[:path]) :  '.'}",
+        "#{options[:file_name] ? options[:file_name] : uri.path.delete_prefix('/')}"
+      )
+      
+      File.open(path, "wb+") do |file|
         file.write Net::HTTP.get(uri)
       end
     end
